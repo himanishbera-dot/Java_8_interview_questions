d59d18ebe5cc8807a1255129bdf6822fb9feda49
himanishbera-dot <himanish.bera@gmail.com>
2026-09-16 18:46:43 +0530

    cimmit 3

--- src/string/ReverseWords.java
+++ src/string/ReverseWords.java
@@ -1,16 +1,19 @@
 package string;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReverseWords {
     public static void main(String[] args) {
 
-        String input = "Java is a high-level, object-oriented programming language.";
+        String input = "Java is a high-level, object oriented programming language.";
 
-        String output = Arrays.stream(input.split(" "))
-                .map(word -> new StringBuilder(word).reverse())
-                .collect(Collectors.joining(" "));
+        List<String> list1 = Arrays.asList(input.split(" "));
+        Collections.reverse(list1);
+
+         String output = list1.stream().collect(Collectors.joining(" "));
 
         System.out.println(output);
     }
